3dfdcab8e3744f5c39095e90ae45806b05c9a8a3
valentin kueck <kueck.valentin@googlemail.com>
2025-03-24 19:26:26 +0100

    add metadata

---
+++ src/app/icon.tsx
@@ -0,0 +1,47 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 64 64"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="32"
+          cy="32"
+          r="30"
+          fill="#fff6ea"
+          stroke="#fff6ea"
+          strokeWidth="2"
+        />
+
+        <path
+          d="M20 20L32 44L44 20"
+          stroke="#cc7900"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
