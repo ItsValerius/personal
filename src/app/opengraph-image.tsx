8dc38be7e54795a24e5fe9d8772c73fb705ef309
valentin kueck <kueck.valentin@googlemail.com>
2025-03-25 11:37:20 +0100

    og image generation

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,32 +1,41 @@
 import { ImageResponse } from "next/og";
-import { join } from "path";
-import { readFile } from "fs/promises";
 
-// Image metadata
 export const alt = "About Me";
 export const size = {
   width: 128,
   height: 128,
 };
-
+export const runtime = "edge";
 export const contentType = "image/png";
 
-// Image generation
 export default async function Image() {
-  const logoData = await readFile(join(process.cwd(), "public/me.jpg"));
-  const logoSrc = Uint8Array.from(logoData).buffer;
-
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-primary">
-        <img src={logoSrc} height="128" width="128" alt="me" />
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-primary">
+          <img
+            src={
+              process.env.VERCEL_PROJECT_PRODUCTION_URL ||
+              `http://localhost:${process.env.PORT || 3000}` + "/me.png"
+            }
+            height="128"
+            width="128"
+            alt="me"
+          />
+        </div>
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
       ...size,
     }
   );
